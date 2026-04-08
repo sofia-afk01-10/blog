b4a672afadab628fd54b1ae1ed0e3a0b61f49391
Sofia <wwww@corp.nstu.ru>
2026-04-08 11:17:32 +0700

    вывод пользователей

--- blog/login/views.py
+++ blog/login/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render, redirect
 from .models import User
 from .forms import UserForm
 
+#страница со списком пользователей
+def users(request):
+    #получим всех пользователей из базы
+    users = User.objects.all()
+    return render(request, 'users.html', {'users': users})
+
 # Create your views here.
 def add_user(request):
     # получили данные. нужно сохранить юзера в базу
@@ -10,7 +16,7 @@ def add_user(request):
         user = UserForm(request.POST)
         if user.is_valid():
             user.save()
-            return redirect('/')
+            return redirect('/users/')
     # это простой запрос, нужно показать форму
     else:
         form = UserForm()
